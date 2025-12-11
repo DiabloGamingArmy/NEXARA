ba5c97cf064c5a492c5d07cb2bb73387e75fe559
DiabloGamingArmy <78702008+DiabloGamingArmy@users.noreply.github.com>
2025-12-11 09:16:53 -0500

    Update thread.js

--- thread.js
+++ thread.js
@@ -1,1 +1,8 @@
-
+/**
+ * MODULE: Thread (Feature Layer)
+ * -------------------------------------------------------------------------
+ * ACTS AS: Recursive Interaction Viewer
+ * RESPONSIBILITY: Rendering hierarchical data structures (comments/replies),
+ * managing parent-child relationships, and "Peer Review" schema logic.
+ * -------------------------------------------------------------------------
+ */
