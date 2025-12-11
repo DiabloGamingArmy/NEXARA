ef366b2080b0bab6aef79c0f315e43295943a8e0
DiabloGamingArmy <78702008+DiabloGamingArmy@users.noreply.github.com>
2025-12-11 09:14:06 -0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,8 @@
-
+/**
+ * MODULE: Utilities (Infrastructure Layer)
+ * -------------------------------------------------------------------------
+ * ACTS AS: Pure Functions & Constants Library
+ * RESPONSIBILITY: Deterministic logic (date formatting, sanitization),
+ * static enums, configuration objects, and asset mapping.
+ * -------------------------------------------------------------------------
+ */
