117a5b807b1877b37f18f78320caa9b5d345399d
DiabloGamingArmy <78702008+DiabloGamingArmy@users.noreply.github.com>
2025-12-11 09:17:16 -0500

    Update discover.js

--- discover.js
+++ discover.js
@@ -1,1 +1,8 @@
-
+/**
+ * MODULE: Discover (Feature Layer)
+ * -------------------------------------------------------------------------
+ * ACTS AS: Taxonomy & Search Engine
+ * RESPONSIBILITY: Client-side filtering/aggregation, category taxonomy
+ * management, and search input debouncing algorithms.
+ * -------------------------------------------------------------------------
+ */
