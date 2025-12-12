8a156c1dfcc99ddfb6d7fec342f43e653cf99a57
DiabloGamingArmy <78702008+DiabloGamingArmy@users.noreply.github.com>
2025-12-12 10:40:46 -0500

    Update Live.js
    Fetching Firestore doc
    
    Subscribing to updates
    
    Mounting/unmounting player

--- Scripts/Live.js
+++ Scripts/Live.js
@@ -1,2 +1,22 @@
-export function initialize() {}
-export function teardown() {}
+onSnapshot(doc(db, "liveStreams", streamId), async snap => {
+    if (!snap.exists()) {
+        player.renderOffline();
+        return;
+    }
+
+    const data = snap.data();
+
+    if (!data.isLive) {
+        player.renderOffline();
+        return;
+    }
+
+    if (!playerLoaded) {
+        await player.load({
+            playbackUrl: data.playbackUrl,
+            visibility: data.visibility,
+            channelArn: data.channelArn,
+        });
+        playerLoaded = true;
+    }
+});
