c1a7ade98f5f4f10b726506629e3b879b0cf3aad
DiabloGamingArmy <78702008+DiabloGamingArmy@users.noreply.github.com>
2025-12-13 18:12:18 -0500

    Stabilize hosting static layout

---
+++ public/scripts/firebase-config.js
@@ -0,0 +1,8 @@
+/**
+ * MODULE: Firebase Config (Infrastructure Layer)
+ * -------------------------------------------------------------------------
+ * ACTS AS: Singleton Service Provider
+ * RESPONSIBILITY: Synchronous instantiation of Firebase SDKs.
+ * EXPORTS: Initialized instances of Auth, Firestore, and Storage.
+ * -------------------------------------------------------------------------
+ */
